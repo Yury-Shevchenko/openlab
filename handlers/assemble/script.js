6c37fcd25db2e3cef52c5efafc689b85e08e3207
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-05-28 10:54:16 +0200

    check label

--- handlers/assemble/script.js
+++ handlers/assemble/script.js
@@ -111,7 +111,7 @@ const processItems = items =>
       } else {
         return ({
           ...i,
-          name: (i.name && i.name.replace(/\./g,'-')) || slugify(i.label.replace(/\./g,'-') || '')
+          name: (i.name && i.name.replace(/\./g,'-')) || slugify((i.label && i.label.replace(/\./g,'-')) || '')
         })
       }
     })
