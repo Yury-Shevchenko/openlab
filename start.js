1f579452007528885ec452ff5b428c83fb73b6ce
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-09 23:13:46 +0200

    useFindAndModify set to false

--- start.js
+++ start.js
@@ -14,7 +14,7 @@ require('dotenv').config({ path: 'variables.env' });
 
 // Connect to Database and handle bad connections
 //mongoose.connect(process.env.DATABASE, {useNewUrlParser: true } );
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useFindAndModify: false });
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
