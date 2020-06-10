5857e65533f5e87c8808e22f7aa832b276abba26
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-10 15:36:14 +0200

    fast saving

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -326,7 +326,6 @@ exports.changeStatusOfDataRequest = async (req, res) => {
 
 //save results during the task
 exports.saveIncrementalResults = async (req, res) => {
-  console.log('329: Starting saving', Date());
   let anonymParticipantId;
   if (!req.user){ anonymParticipantId = req.body.url.split('/')[5] };
 
@@ -337,13 +336,11 @@ exports.saveIncrementalResults = async (req, res) => {
 
   const openLabId = (req.user && req.user.openLabId) || anonymParticipantId || "undefined";
   const projectId = (req.user && req.user.participantInProject) || (req.user && req.user.project._id) || test.project || "undefined";
-  console.log('340: Created variables', Date());
 
   // todo why do we need project and parameters?
   const project = await Project.findOne({ _id: projectId },{
     name: 1, osf: 1, parameters: 1,
   });
-  console.log('346: Found project', Date());
 
   if(req.body.data && req.body.data.length !== 0){
     req.body.data.map(row => {
@@ -355,13 +352,10 @@ exports.saveIncrementalResults = async (req, res) => {
       row['code'] = (req.user && req.user.code && req.user.code.id) || openLabId;
     })
   };
-  console.log('358: Updated datafile', Date());
 
   if (req.body.metadata.payload == 'incremental' && project && project.osf && project.osf.policy !== 'OSF'){
-    let result = await Result.find({transfer: req.body.metadata.id, uploadType: req.body.metadata.payload}, { _id:1, author:0 }).limit(1);
-    // that takes a lot of time to find the result
-    console.log('362: Found result', Date());
-    if(result.length === 0){
+    const results = await Result.find({transfer: req.body.metadata.id, uploadType: req.body.metadata.payload}, { _id:1, author:0 }).limit(1);
+    if(results.length === 0){
       const parameters = await Param.getParameters({
         slug: slug,
         language: (req.user && req.user.language) || req.body.url.split('/')[5] || "english",
@@ -386,30 +380,15 @@ exports.saveIncrementalResults = async (req, res) => {
         uploadType: req.body.metadata.payload,
         parameters: params
       });
-      console.log('388: Created new result', Date());
       await result.save();
-      console.log('390: Saved new result', Date());
     } else {
-      console.log('392: Start to update existing result', Date());
-      // const updatedResult = await Result.findOneAndUpdate({
-      //   transfer: req.body.metadata.id,
-      //   uploadType: req.body.metadata.payload
-      // }, { $push: { rawdata: { $each: req.body.data } }}, {
-      //   new: true
-      // }).exec();
-      // that takes a lot of time to find the result
       await Result.updateOne({
         transfer: req.body.metadata.id,
         uploadType: req.body.metadata.payload
       }, { $push: { rawdata: { $each: req.body.data } }}, {});
-
-      console.log('399: Updated existing result', Date());
     };
     res.send('saved');
-    console.log('402: After sending saved - should not be shown', Date());
-
   } else if(req.body.metadata.payload == 'full'){
-
     const parameters = await Param.getParameters({
       slug: slug,
       language: (req.user && req.user.language) || req.body.url.split('/')[5] || "english",
@@ -492,7 +471,6 @@ exports.saveIncrementalResults = async (req, res) => {
   } else {
     res.send('Nothing was saved');
   }
-  console.log('488: Out of the scope - should not be shown', Date());
 };
 
 //show the results for each test
