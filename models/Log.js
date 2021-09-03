190a12650479380d36bd953b7b6702041a1e9663
Yury-Shevchenko <shevchenko_yury@mail.ru>
2021-09-03 16:00:42 +0200

    change in Log

--- models/Log.js
+++ models/Log.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const logSchema = new mongoose.Schema({
   created: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   type: String,
   author: {
     type: mongoose.Schema.ObjectId,
     ref: 'User',
   },
-  project:{
+  project: {
     type: mongoose.Schema.ObjectId,
-    ref: 'Project'
+    ref: 'Project',
   },
   test: {
     type: mongoose.Schema.ObjectId,
     ref: 'Test',
   },
-})
+});
 
 // define indexes for the faster search
 logSchema.index({
